ee4dad8aec3a4df801e556be4f29c2ab61ccbc9b
daan <2087884@talnet.nl>
2020-02-13 12:28:00 +0100

    eindelijk klaar

--- wisselgeld.php
+++ wisselgeld.php
@@ -3,40 +3,33 @@
 
 $input = $argv[1];
 $getal = intval($input);
+$tienHoeveel = 0;
+$vijfHoeveel = 0;
+$tweeHoeveel = 0;
+$eenHoeveel = 0;
 
-switch($getal){
 
-    case 50:
-        $vijftig = ($getal % 50);
-        $overVijftig = floor($vijftig);
-    break;
 
-    case 20:
-        $twintig = ($overVijftig % 20);
-        $overtwintig = floor($twintig);
-    break;
-
-    case 10:
-        $tien = ($overtwintig % 10);
-        $overtien = floor($tien);
-    break;
-
-    case 5:
-        $vijf = ($overtien % 5);
-        $overvijf = floor($vijf);
-    break;
-
-    case 2:
-        $twee = ($overvijf % 2);
-        $overtwee = floor($twee);
-    break;
-
-    case 1:
-        $een = ($overtwee % 1);
-        $overeen = floor($een);
-    break;
+if( $getal >= 10){
+    $tienHoeveel = floor($getal / 10);
+    $getal = ($getal - ($tienHoeveel*10));
+    echo($tienHoeveel." x 10 euro".PHP_EOL);
+}
+if( $getal >= 5){
+    $vijfHoeveel = floor($getal / 5);
+    $getal = ($getal - ($vijfHoeveel*5));
+    echo($vijfHoeveel." x 5 euro".PHP_EOL);
+}
+if( $getal >= 2){
+    $tweeHoeveel = floor($getal / 2);
+    $getal = ($getal - ($tweeHoeveel*2));
+    echo($tweeHoeveel." x 2 euro".PHP_EOL);
 
 }
-
+if( $getal >= 1){
+    $eenHoeveel = floor($getal / 1);
+    $getal = ($getal - ($eenHoeveel*1));
+    echo($eenHoeveel." x 1 euro".PHP_EOL);
+}
 
 ?>
