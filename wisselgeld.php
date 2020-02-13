5f333ce1c63f3ecd7f0473203764fc7f706d65a6
daan <2087884@talnet.nl>
2020-02-13 10:39:03 +0100

    verandering

--- wisselgeld.php
+++ wisselgeld.php
@@ -7,34 +7,35 @@ $getal = intval($input);
 switch($getal){
 
     case 50:
+        $vijftig = ($getal % 50);
+        $overVijftig = floor($vijftig);
     break;
 
     case 20:
+        $twintig = ($overVijftig % 20);
+        $overtwintig = floor($twintig);
     break;
 
     case 10:
+        $tien = ($overtwintig % 10);
+        $overtien = floor($tien);
     break;
 
     case 5:
+        $vijf = ($overtien % 5);
+        $overvijf = floor($vijf);
     break;
 
     case 2:
+        $twee = ($overvijf % 2);
+        $overtwee = floor($twee);
     break;
 
     case 1:
+        $een = ($overtwee % 1);
+        $overeen = floor($een);
     break;
 
-    case 0.50:
-    break;
-
-    case 0.20:
-    break;
-
-    case 0.10:
-    break;
-
-    case 0.05:
-    break;
 }
 
 
